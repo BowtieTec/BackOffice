506f2d058fd50154ab1e72813b400c1b25858874
aleja <alejandrocordova198@gmail.com>
2022-08-22 12:26:32 -0600

    Added relation: Admin Has Courtesy

--- src/app/shared/model/CommonModels.ts
+++ src/app/shared/model/CommonModels.ts
@@ -3,13 +3,18 @@ export class SelectModel {
   name = ''
 }
 
-export interface  ListCheckModel {
-  id:string,
-  name:string
+export interface ListCheckModel {
+  id: string,
+  name: string
   isChecked: boolean
   disable: boolean
 }
 
-export interface listID{
-  id:string
+export interface listID {
+  id: string
+}
+
+export interface ISelectModel {
+  id: string | number
+  name: string;
 }
