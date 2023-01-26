a0274d5a96433cca4c9ad288420f1f481e59600b
Alejandro Cordova <alejandrocordova198@gmail.com>
2023-01-26 13:43:41 -0600

    Reuse component from panel and title

--- src/app/shared/panel-sub-module/panel-sub-module.component.ts
+++ src/app/shared/panel-sub-module/panel-sub-module.component.ts
@@ -3,13 +3,12 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-panel-sub-module',
   template: `
-    <div class = "row justify-content-center ">
-      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12 animate__animated animate__fadeInLeft">
+    <div class = "row justify-content-center">
+      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12 animate__animated animate__fadeInLeft ">
         <div class = "panels">
-          <div class = "titleCards">
-            <span><em class = "{{icon}}"></em></span> {{title}}
-            <ng-content></ng-content>
-          </div>
+          <app-title-cards [icon] = "icon"
+                           [title] = "title"></app-title-cards>
+          <ng-content></ng-content>
         </div>
       </div>
     </div>
