62a16424e33086c5de8e6a110bf292ae316adbc0
Alejandro Cordova <alejandrocordova198@gmail.com>
2023-02-01 15:01:07 -0600

    Change the forms to be managed by panel component.

--- src/app/shared/panel-sub-module/panel-sub-module.component.ts
+++ src/app/shared/panel-sub-module/panel-sub-module.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-panel-sub-module',
   template: `
     <div class = "row justify-content-center">
       <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12 animate__animated animate__fadeInLeft ">
-        <div class = "panels row">
+        <div class = "panels row"
+             [attr.aria-current] = "formGroup? formGroup: null">
           <app-title-cards [icon] = "icon"
                            [title] = "title"></app-title-cards>
           <ng-content></ng-content>
@@ -18,4 +20,7 @@ import {Component, Input} from '@angular/core';
 export class PanelSubModuleComponent {
   @Input() title: string = '';
   @Input() icon: string = '';
+  @Input() formGroup!: FormGroup
+  @Input() submitFunc: () => void = () => {
+  }
 }
